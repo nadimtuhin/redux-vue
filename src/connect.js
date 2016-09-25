596990be26d4b880425f105117e5c5b8cf0a9fa4
Nadim Tuhin <nadimtuhin@gmail.com>
2016-09-25 21:02:02 +0600

    throw error if props are passed as array

--- src/connect.js
+++ src/connect.js
@@ -1,8 +1,14 @@
 import noop from 'lodash/noop';
 import omitBy from 'lodash/omitBy';
+import isArray from 'lodash/isArray';
 
 export default function connect(mapStateAsProps = noop, mapActionsAsProps = noop) {
     return (children) => {
+        
+        if(isArray(children.props)){
+            throw Error(`Vua Redux Connected Component can not have props as array component = ${JSON.stringify(component, null, 2)}`)
+        }
+
         /** @namespace children.collect */
         const props = children.props || {};
         const propsToPass = Object.keys(props).map(key => `:${key}='${key}'`).join(' ');
