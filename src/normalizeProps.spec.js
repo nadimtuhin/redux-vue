5322aa2ca8f95c463d21f1ef677d5de439d62aca
Nadim Tuhin <nadimtuhin@gmail.com>
2016-09-26 20:32:52 +0600

    normolizeProps spec added

--- src/normalizeProps.spec.js
+++ src/normalizeProps.spec.js
@@ -2,7 +2,12 @@ import expect from 'expect';
 import normalizeProps from './normalizeProps';
 
 describe('normalize props', () => {
-	it('should normalize props', () => {
-		console.log(normalizeProps(['a', 'b']));
+	it('should normalize array props', () => {
+		expect(normalizeProps(['a', 'b'])).toEqual({a: null, b: null});
+	});
+
+	it('should normalize object props', () => {
+		const props = {'a': {type: String}, 'b': null};
+		expect(normalizeProps(props)).toEqual(props);
 	});
 });
