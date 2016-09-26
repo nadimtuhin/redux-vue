0114276c3afaf6a29c4577106d090c88299d40e8
Nadim Tuhin <nadimtuhin@gmail.com>
2016-09-26 20:24:40 +0600

    added tests

---
+++ src/normalizeProps.spec.js
@@ -0,0 +1,8 @@
+import expect from 'expect';
+import normalizeProps from './normalizeProps';
+
+describe('normalize props', () => {
+	it('should normalize props', () => {
+		console.log(normalizeProps(['a', 'b']));
+	});
+});
